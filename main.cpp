e81489d0d0a066539610bbadf08b5897bf8ad39d
ejacintogUD <159667541+ejacintogUD@users.noreply.github.com>
2024-08-26 13:57:42 -0500

    Add files via upload
    Se genera codigo con alias y funciones para mayor entendimiento

--- main.cpp
+++ main.cpp
@@ -3,63 +3,93 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+ // Version 2024-3 
+ // Manejo de 8 leds usando un registro de corrimiento 74HC595
+
 #include "AnalogIn.h"
+#include "DigitalOut.h"
+#include "PinNameAliases.h"
+#include "PinNames.h"
 #include "ThisThread.h"
+#include "Thread.h"
+#include "cmsis_os2.h"
 #include "mbed.h"
 
-//#define leds 3
+#define DS   D4 // alias de los pines 
+#define SHCP D2
+#define STCP D3
 
-DigitalOut Ds(D2);    // Entrada serial del registro de corrimiento
-DigitalOut SHCP(D3); // Clock flanco de subida
-DigitalOut STCP(D4); // Latch flanco de bajada
+//#define salidas para manejo del registro de corrimiento
+DigitalOut SerialData (DS);     // Datos seriales a correr
+DigitalOut RegisterClk(SHCP);   // Reloj del registro de corrimiento
+DigitalOut LatchClk   (STCP);   // Reloj del Latch del registro
+DigitalOut led(LED1);
 
-AnalogIn Ain (A0);
 
-int i;
-float Vin;
-int leds=0;
+//AnalogIn Ain (A0); // Entrada analoga para el Vúmetro
 
+
+//float Vin;  // Float donde se carga la entrada analoga
+char leds=0;  //Variable donde se cargan los datos    
+
+
+//protipos 
+void shift_data (void);
+void load_data (void);
+void send_data(void); // funcion del hilo
+
+
+
+// hilos y herramientas del sistema operativo
+Thread T_send_data(osPriorityNormal, 4096, NULL, NULL);
 
 
 int main()
 {
-
-    Ds=0;
-    SHCP =0;
-    STCP=1;
-
-     for(i=0; i<8; i++)
+    // Arranque del programa
+    SerialData  =0;
+    RegisterClk =0;
+    LatchClk    =0;
+    // Encender los hilos
+    T_send_data.start(send_data);
+    while (true) 
         {
-            Ds=0;
-            SHCP = 0;
-            STCP = 1;
-            wait_us(1);
-            SHCP = 1;
-            STCP = 0;
-            wait_us(1);
-            
-        }
-
-    while (true) {
-        Vin = Ain*7;
-        leds = int(Vin);
-
-        for(i=0; i<8; i++)
-        {
-            if (i< leds-1) Ds = 1;
-            else Ds=0;
-            SHCP = 0;
-            STCP = 1;
-            wait_us(10);
-            SHCP = 1;
-            STCP = 0;
-            wait_us(10);
-           
+           led = !led;
+           ThisThread::sleep_for(1s);
         }
        
-        ThisThread::sleep_for(100ms);
+      
+}
 
+void shift_data (int8_t data)
+{
+     for(int i=0; i<8; i++)
+        {
+            data = data>>1;
+            //if ((data & 0x01) == 0x01) SerialData = 1;
+            //else                       SerialData = 0;              
+            SerialData = ((data & 0x01) == 0x01) ? 1:0;
+            RegisterClk=0;
+            wait_us(1);
+            RegisterClk=1;
+            wait_us(1); 
+        }
+}
 
+void load_data(int8_t data)
+{
+    LatchClk=0;
+    shift_data(data);
+    LatchClk=1;    
+}
 
+void send_data (void)
+{
+    while(true)
+    {
+        load_data(0x55);
+        ThisThread::sleep_for(1s);
     }
 }
+
+
